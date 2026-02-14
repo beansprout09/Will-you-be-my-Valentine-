3b8a4d243108ffcc85a202611cf5f155452f77b6
haley <3070674T@student.gla.ac.uk>
2026-02-14 23:44:10 +0800

    Update Valentine's page

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "oops, try again!",
+    "nope",
+    "i sell ur pokemon card",
+    "bitch",
+    "nah",
+    "no",
+    "nope",
+    "oops",
+    "ill be trent",
+    "i sell ur pokemon card❤️"
 ];
 
 let messageIndex = 0;
